6a4a8d9309d2b9d9c9599de237ca65bc48606374
OllyBaru <art.baru16@gmail.com>
2024-05-25 19:59:36 +0000

    UpGameServerRelease

---
+++ ServerRelease/StartServers.bat
@@ -0,0 +1,21 @@
+cd .\Messenger\
+start .\Messenger.exe
+cd ..\
+cd .\Helper\
+start .\Helper.exe
+cd ..\
+cd .\SubHelper\
+start SubHelper.exe
+cd ..\
+cd .\Billing\
+start .\ReZasCashServer.exe
+cd ..\
+cd .\Connector\
+start .\Connector.exe
+cd ..\
+cd .\Login\
+start LoginServer.exe
+cd ..\
+
+cd .\Gameserver\
+start Gameserver.exe
---
+++ ServerRelease/killlc.bat
@@ -0,0 +1,9 @@
+
+taskkill -f -im gameserver.exe
+taskkill -f -im connector.exe
+taskkill -f -im helper.exe
+taskkill -f -im loginserver.exe
+taskkill -f -im subhelper.exe
+taskkill -f -im messenger.exe
+taskkill -f -im ReZasCashServer.exe
+taskkill -f -im powershell.exe
